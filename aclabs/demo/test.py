60a925b5e6a93b730603d7ba3bc054ca1bb2c688
Calin Rodina <calinrodina@gmail.com>
2016-05-18 18:19:29 +0300

    Test commit

---
+++ aclabs/demo/test.py
@@ -0,0 +1,33 @@
+#another comment
+#My first python app
+''' 
+    some comment here
+'''
+
+a = 10
+
+my_string = 'test'
+my_string = 'test2'
+# 'test' now destroyed by garbage collector
+
+
+# c = int(raw_input('Number: '))
+# print('Your number  %s' % c)
+# print(c + a)
+
+my_dict = {'test' : 10,
+            'abc' : [1, 2, 'abbb', 'test3'],
+            'c' : dict(a=10),
+            'cd' : {'a': 10}}
+
+# print my_dict
+# print my_dict['abc']
+# my_dict['c'] = 'a'
+#prints all functions of my_dict
+#print dir(my_dict)
+
+# a = range(10)
+# print a[2:-1: 2]
+
+
+
